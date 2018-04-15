2ae4db70bbe9a425da8539b21abcda027a7bd424
Terry Haynie <terryhaynie@att.net>
2018-04-15 01:32:43 -0500

    The project works completely! FINAL submission.

--- main.py
+++ main.py
@@ -19,20 +19,20 @@ def welcome_user():
 
     if len(username) < 3 or len(username) > 20  or " " in username:
         error1 = "Please enter a valid username."
-        return render_template("edit.html",  error1=error1 )
+        return render_template("edit.html", email=email,  error1=error1 )
     
     if len(password) < 3 or len(password) > 20 or " " in password:
         error2 = "Please enter a valid password."
-        return render_template("edit.html", error2 =error2)
+        return render_template("edit.html",name=username, email=email, error2 =error2)
 
     if password_copy != password:
         error3 = "Please make sure your password matches your confirmation password"
-        return render_template("edit.html", error3=error3)
+        return render_template("edit.html", name = username, email=email, error3=error3)
     
     if email:
-        if len(email) < 3 or len(email) > 20 or " " in email:
+        if len(email) < 3 or len(email) > 20 or " " in email or email.count('@') > 1 or email.count('.') > 1:
             error4 = "Please enter a valid email address."
-            return render_template("edit.html", error4=error4)
+            return render_template("edit.html", name=username, error4=error4)
 
      
 
